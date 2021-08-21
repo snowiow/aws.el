313b5f94049a60ec5f6246169a340c62c82e3ec9
snowiow <marcel.patzwahl@posteo.de>
2021-08-21 18:21:09 +0200

    Add GitHub Actions Test

---
+++ test/aws-core-test.el
@@ -0,0 +1,15 @@
+;;; package --- Summary
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'ert)
+
+(ert-deftest aws--test-buffer-name ()
+  (let ((aws-profile "default")
+        (aws--current-service "lambda"))
+    (should (equal "*aws.el [profile: default] [service: lambda]*" (aws--buffer-name)))))
+
+(provide 'aws-core-test)
+;;; aws-core-test.el ends here
