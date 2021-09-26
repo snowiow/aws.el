37a4d0c36972913de3b3a45bfa6688287090ea73
snowiow <marcel.patzwahl@posteo.de>
2021-09-26 10:54:23 +0200

    Revert "Override package archives to not use orgmodes elpa repo"
    This reverts commit 69b72eabe210aa5ae0ec4d20b68f682458b43b17.

--- .github/workflows/install-deps.el
+++ .github/workflows/install-deps.el
@@ -1,9 +1,6 @@
 (require 'package)
 
-(setq package-archives
-      '(("gnu" . "http://elpa.gnu.org/packages/")
-        ("melpa" . "https://melpa.org/packages/")))
-
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 (package-refresh-contents)
 
